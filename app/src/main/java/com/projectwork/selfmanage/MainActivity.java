711c4b45313fdb9f7c92a00bce74132673e1695d
Svetlana Guma <dunno183@gmail.com>
2017-09-15 21:08:35 +0300

    Экран добавления нового задания

--- app/src/main/java/com/projectwork/selfmanage/MainActivity.java
+++ app/src/main/java/com/projectwork/selfmanage/MainActivity.java
@@ -1,15 +1,18 @@
 package com.projectwork.selfmanage;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView tViewMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +21,23 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        tViewMain = (TextView) findViewById(R.id.tViewMain);
+
        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, AddTask.class);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data == null) {return;}
+        String result = data.getStringExtra("result");
+        tViewMain.setText(result);
     }
 
     @Override
