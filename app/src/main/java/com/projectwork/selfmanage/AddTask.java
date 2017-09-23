ab31dd0f6ca576b2e13df6e3ee87692d4c94bb1f
alinamorneva <alina.morneva@mail.ru>
2017-09-23 15:04:15 +0300

    menu

---
+++ app/src/main/java/com/projectwork/selfmanage/AddTask.java
@@ -0,0 +1,60 @@
+package com.projectwork.selfmanage;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class AddTask extends AppCompatActivity implements View.OnClickListener {
+
+    Button saveBtn, cancelBtn;
+    EditText eText;
+    TextView testTView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_task);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        });
+
+        saveBtn = (Button) findViewById(R.id.saveBtn);
+        cancelBtn = (Button) findViewById(R.id.cancelBtn);
+
+        eText = (EditText) findViewById(R.id.activityName);
+
+        testTView = (TextView) findViewById(R.id.textView);
+
+        saveBtn.setOnClickListener(this);
+        cancelBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.saveBtn:
+                Intent intent = new Intent();
+                intent.putExtra("result", eText.getText().toString());
+                setResult(RESULT_OK, intent);
+                finish();
+                break;
+            case R.id.cancelBtn:
+                finish();
+                break;
+        }
+    }
+}
