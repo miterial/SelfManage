86958c2097b0a16f0100846599da0c468b45d763
Svetlana Guma <dunno183@gmail.com>
2017-10-02 19:00:18 +0300

    Присвоение значений полям Task

--- app/src/main/java/com/projectwork/selfmanage/AddTask.java
+++ app/src/main/java/com/projectwork/selfmanage/AddTask.java
@@ -6,14 +6,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class AddTask extends AppCompatActivity implements View.OnClickListener {
 
     Button saveBtn, cancelBtn;
-    EditText eText;
+    EditText eTextName, eTextDateTime, eTextDuration;
     TextView testTView;
+    CheckBox chbRepeat, chbTermless;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +37,13 @@ public class AddTask extends AppCompatActivity implements View.OnClickListener {
         saveBtn = (Button) findViewById(R.id.saveBtn);
         cancelBtn = (Button) findViewById(R.id.cancelBtn);
 
-        eText = (EditText) findViewById(R.id.taskName);
+        eTextName = (EditText) findViewById(R.id.taskName);
+        eTextDateTime = (EditText) findViewById(R.id.eTextDateTime);
+        eTextDuration = (EditText) findViewById(R.id.eTextDuration);
+        chbRepeat = (CheckBox) findViewById(R.id.chBrepeat);
+        chbTermless = (CheckBox)  findViewById(R.id.chBtermless);
 
-        testTView = (TextView) findViewById(R.id.textView);
+        //testTView = (TextView) findViewById(R.id.textView);
 
         saveBtn.setOnClickListener(this);
         cancelBtn.setOnClickListener(this);
@@ -45,10 +51,29 @@ public class AddTask extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
+        String res = "";
+        StringBuilder sb = new StringBuilder(100);
         switch (v.getId()) {
             case R.id.saveBtn:
+                // Получение параметров задания
+                sb.append(eTextName.getText().toString());
+                sb.append(",");
+                sb.append(eTextDateTime.getText().toString());
+                sb.append(",");
+                sb.append(eTextDuration.getText().toString());
+                sb.append(",");
+                if(chbRepeat.isChecked())
+                    sb.append(1);
+                else sb.append(0);
+                sb.append(",");
+                if(chbTermless.isChecked())
+                    sb.append(1);
+                else sb.append(0);
+
+                // Пересылка на главный экран
+                //TODO: Добавление данных в базу
                 Intent intent = new Intent();
-                intent.putExtra("result", eText.getText().toString());
+                intent.putExtra("result", sb.toString());      //Возврат названия задачи на главный экран
                 setResult(RESULT_OK, intent);
                 finish();
                 break;
